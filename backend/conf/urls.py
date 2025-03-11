37bbb59fc36c53ae518b9232c5022c4055052c2a
fabichoi <fabianus.c@gmail.com>
2025-03-11 15:18:18 +0900

    MOD: EC2 설정 및 스웨거 설정

--- backend/conf/urls.py
+++ backend/conf/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path('swagger/', SpectacularSwaggerView.as_view(), name="swagger-ui"),
     path('', include("apis.urls")),
 ]
